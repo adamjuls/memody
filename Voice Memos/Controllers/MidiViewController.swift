c53f7d42318508d52bac7a30a9deefd254a106c0
Lauren Friedrich <lfriedr@umich.edu>
2023-03-20 13:49:03 -0400

    MidiViewController

---
+++ Voice Memos/Controllers/MidiViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MidiViewController.swift
+//  Voice Memos
+//
+//  Created by Lauren Friedrich on 3/20/23.
+//  Copyright © 2023 Shahzaib. All rights reserved.
+//
+
+import UIKit
+
+class MidiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
